7d2c96fcd9989f4850e6e5243008a1b9fed8ed2a
notjrbauer <notjrbauer@gmail.com>
2015-12-21 00:46:57 -0500

    Functional
    - add ap module
    - make index.js more fun-fun-functional

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 'use strict'
 
 var Promise = require('bluebird')
+var curry = require('ap').curry
 var assert = require('assert')
 
 module.exports = promiseWaterfall
 
-function promiseWaterfall (callbacks) {
-  assert(callbacks, 'missing argument promiseWaterfall.callbacks')
-  if (!Array.isArray(callbacks)) return promiseWaterfall([callbacks])
-
-  return callbacks.reduce(waterfall, Promise.resolve())
+var waterfall = function waterfall (promise, callback) {
+  return promise.then(callback)
 }
 
-function waterfall (promise, callback) {
-  return promise.then(callback)
+var reduce = curry(function reduce (fn, value) {
+  return value.reduce(fn, Promise.resolve())
+})
+
+var reduceWaterfall = reduce(waterfall)
+
+function promiseWaterfall (callbacks) {
+  assert(callbacks, 'missing argument promiseWaterfall.callbacks')
+
+  if (!Array.isArray(callbacks)) return promiseWaterfall([callbacks])
+  return reduceWaterfall(callbacks)
 }
