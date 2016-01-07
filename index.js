0e765dd34edd01d0a8fc0f4e843dc0cda2b8b173
Ben Drucker <bvdrucker@gmail.com>
2016-01-07 15:17:34 -0800

    Use reducer to simplify serial promise resolution

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 'use strict'
 
-var curry = require('ap').curry
-var assertOk = require('assert-ok')
+var assert = require('assert-ok')
+var array = require('cast-array')
 
-module.exports = promiseWaterfall
+module.exports = function promiseWaterfall (callbacks) {
+  assert(callbacks, 'promiseWaterfall requires an array of callbacks')
 
-var waterfall = function waterfall (promise, callback) {
-  return promise.then(callback)
-}
-
-var reduce = curry(function reduce (fn, value) {
-  return value.reduce(fn, Promise.resolve())
-})
-
-var reduceWaterfall = reduce(waterfall)
-
-function promiseWaterfall (callbacks) {
-  assertOk(callbacks, 'missing argument promiseWaterfall.callbacks')
-
-  if (!Array.isArray(callbacks)) return promiseWaterfall([callbacks])
-  return reduceWaterfall(callbacks)
+  return array(callbacks).reduce(function (accumulator, promise) {
+    return accumulator.then(promise)
+  }, Promise.resolve())
 }
