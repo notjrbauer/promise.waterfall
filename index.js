757187d2a3aa610bd22a4691200e5bf82fb7728f
notjrbauer <notjrbauer@gmail.com>
2016-01-08 17:09:37 -0800

    @ajoslin merge

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 'use strict'
 
-var curry = require('ap').curry
-var assertOk = require('assert-ok')
-
 module.exports = promiseWaterfall
 
-var waterfall = function waterfall (promise, callback) {
-  return promise.then(callback)
-}
-
-var reduce = curry(function reduce (fn, value) {
-  return value.reduce(fn, Promise.resolve())
-})
-
-var reduceWaterfall = reduce(waterfall)
-
 function promiseWaterfall (callbacks) {
-  assertOk(callbacks, 'missing argument promiseWaterfall.callbacks')
-
-  if (!Array.isArray(callbacks)) return promiseWaterfall([callbacks])
-  return reduceWaterfall(callbacks)
+  return callbacks.slice(1).reduce(function (accumulator, promise) {
+    return accumulator.then(promise)
+  }, callbacks[0]())
 }
